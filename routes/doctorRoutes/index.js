30b31e11e371a8e6e8af74c4c7305ec7eae68447
Nikhil <nilkhilop1998@gmail.com>
2024-03-20 10:01:18 +0530

    ADDING DOCTORROUTES

--- routes/doctorRoutes/index.js
+++ routes/doctorRoutes/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import Doctor from "../../db/model/doctorScehma.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 
 const router = express.Router();
@@ -11,17 +13,34 @@ router.post('/signup', async (req,res)=>{
     if(doctor){
         return res.status(403).json({message:"Username already exist"})
     }
-    if(body.password !== body.conformPassword){
+    if(body.password !== body.confirmPassword){
         return res.status(422).json({ message: "Password does not match" });
     }
     const hashedPassword =  await bcrypt.hash(body.password, 2);
     body.password = hashedPassword;
+
     await Doctor.create(body);
     return res.status(201).json({ message: "Signup sucessfull" });
 })
 
-router.post("/login", (req, res) => {
-    return res.status(200).json({ message: "Login sucessfull" });
-});
+router.post("/login", async (req, res) => {
+    const body = {...req.body};
+    const doctor = await Doctor.findOne({username : body.username});
+    if (!doctor) {
+        return res.status(403).json({ message: "Username or Password incorrect" });
+    }
+    const isMatching = await bcrypt.compare(body.password,doctor.password);
+    if(!isMatching){
+    return res.status(403).json({ message: "Username or Password incorrect" });
+    }
+    const secret_key = 'dfnb984h2398fun3389er48rhfhd3h82398r4h2487^ghf474f872ubr363rg237rg243r7g24388gr7';
+    
+    const token = jwt.sign({role :'DOCTOR',id :doctor._id},secret_key,{
+        expiresIn:'7d',
+    })
+    return res.status(200).json({ message: "Login sucessfull" ,token :token});
+    });
+
+
 
 export default router;
--- routes/userRoutes/index.js
+++ routes/userRoutes/index.js
@@ -1,33 +1,50 @@
-import express from 'express';
+import express from "express";
 import User from "../../db/model/userSchema.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 
 const router = express.Router();
 
-router.post('/signup', async (req,res) =>{
-    const body = {...req.body};
+
+
+router.post("/signup", async (req, res) => {
+    const body = { ...req.body };
+
+    const user = await User.findOne({ username: body.username });
+    if (user) {
+        return res.status(403).json({ message: "username already taken" });
+    }
+    if (body.password !== body.confirmPassword) {
+        return res.status(403).json({ message: "password does not match" });
+    }
+    const hashPassword = await bcrypt.hash(body.password, 2);
+    body.password = hashPassword;
+
+    await User.create(body);
+
+    return res.status(201).json({ message: "signup successfull" });
+});
+
+
+
+    router.post("/login", async (req, res) => {
+        const body = {...req.body};
     const user = await User.findOne({username : body.username});
-
-    if(user){
-        return res.status(403).json({message:'User already exist'});
+    if (!user) {
+        return res.status(403).json({ message: "Username or Password incorrect" });
     }
-    if(body.password !== body.conformPassword){
-        return res.status(403).json({ message: "Password doesnot match!" });
+    const isMatching = await bcrypt.compare(body.password,user.password);
+    if(!isMatching){
+    return res.status(403).json({ message: "Username or Password incorrect" });
     }
+    const secret_key = 'dfnb984h2398fun3389er48rhfhd3h82398r4h2487^ghf474f872ubr363rg237rg243r7g24388gr7';
+    
+    const token = jwt.sign({role :'USER',id :user._id},secret_key,{
+        expiresIn:'7d',
+    })
+    return res.status(200).json({ message: "Login sucessfull" ,token :token});
+    });
 
-    const hashedPassword = await bcrypt.hash(body.password, 2);
-        body.password = hashedPassword;
-        await Doctor.create(body);
-        
-    return res.status(201).json({message : 'Successfuly signup'})
-})
-
-router.post('/login' ,async (req,res) =>{
-    res.status(200).json({message:'Login successfuly'});
-})
-
-
-
-
-export default router;
+    export default router;
